55d69b3b58791be376a1058568663bc3acf8bb49
faithadelokun2021 <faithadelokun2021@gmail.com>
2023-03-07 20:28:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+//document.getElementById("count-el").innerText = 5
+
+// let count = 5
+// count = count + 1
+// console.log(count)
+
+
+// function increment() {
+//     console.log("The button was clicked")
+// }
+
+// function countdown() {
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+
+// countdown()
+
+// countdown()
+
+// function log() {
+//     console.log(42)
+// }
+// log()
+
+
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// function lap() {
+//     let totalTime = lap1 + lap2 + lap3
+//     console.log(totalTime)
+// }
+// lap()
+
+
+// let lapscompleted = 0
+// function incrementLap() {
+//     lapscompleted = lapscompleted + 1
+// }
+
+// incrementLap()
+// incrementLap()
+// incrementLap()
+// console.log(lapscompleted)
+
+
+
+
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+let count = 0
+
+function increment() {
+
+    count = count +1
+    countEl.innerText = count
+}
+
+function save() {
+    console.log(count)
+}
+
+
+
+
+// let username = "per"
+
+// let message = "You have three messages"
+
+// let messageToUser = message + ", " + username + "!"
+
+// console.log(messageToUser)
+
+
+// let name = "18"
+
+// let greeting = "Good morning "
+
+// let myGreeting = name + ", " + greeting
+
+// console.log(myGreeting)
+
+// console.log("2" + "4")
